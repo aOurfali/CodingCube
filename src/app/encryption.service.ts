446e7941e79cbef2ad986bd53ebe6564afdd720d
Abdullah Ourfali <80325019+aOurfali@users.noreply.github.com>
2023-02-10 16:38:18 +0100

    added graphql module

--- src/app/encryption.service.ts
+++ src/app/encryption.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
